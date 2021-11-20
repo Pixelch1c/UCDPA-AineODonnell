4ab508fa4c6c8cc13282593e3dec0b53ce364388
DavCon5 <89776476+DaveAITGit@users.noreply.github.com>
2021-11-20 20:40:35 +0000

    FredAPI and packages install
    After install and import of numpy, pandas, matplotlib, datareader and fredapi

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plot
+import matplotlib.pyplot as plt
+import pandas_datareader.data
+
+from fredapi import Fred
+fred=(api_key='52c96fa9db703fe0764c2589b15239d7')
+from pandas_datareader.data import DataReader
+from datetime import date
+
+freddata = Fred.get_series('')
 
 pd.read_csv()

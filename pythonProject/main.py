2985b94b7c53965c1e698ddcfebfce09f4e45ea6
DavCon5 <89776476+DaveAITGit@users.noreply.github.com>
2021-11-22 00:21:31 +0000

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -3,12 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas_datareader.data
+import DateTime
 from fredapi import Fred
 
 fred = Fred(api_key='52c96fa9db703fe0764c2589b15239d7')
+series = 'BOGZ1FL073164003Q'
+start = date(2000, 1, 1)
+stkdata = DataReader(series, 'fred', start)
 
-LTGBdata = fred.get_series('IRLTLT01IEM156N')
-
-print(LTGBdata.head())
-print (LTGBdata.describe())
-
+stkdata.info()

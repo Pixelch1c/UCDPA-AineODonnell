9a0902198450cec177cd47df4e0998bd0386c9be
Pixelch1c <seisiunist@hotmail.com>
2021-11-24 13:26:18 +0000

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -7,8 +7,4 @@ import DateTime
 from fredapi import Fred
 
 fred = Fred(api_key='52c96fa9db703fe0764c2589b15239d7')
-series = 'BOGZ1FL073164003Q'
-start = date(2000, 1, 1)
-stkdata = DataReader(series, 'fred', start)
-
-stkdata.info()
+fred.get_series('BOGZ1FL073164003Q')

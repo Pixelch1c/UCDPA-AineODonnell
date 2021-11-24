55b5cabbc2b75cb55510c4bfdfd21505bf41799c
Pixelch1c <seisiunist@hotmail.com>
2021-11-24 15:27:22 +0000

    Update main.py

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from fredapi import Fred
 
-#connecting to an online API
+# connecting to an online API
 fred = Fred(api_key='52c96fa9db703fe0764c2589b15239d7')
 freddata = fred.get_series('BOGZ1FL073164003Q')
 
 print(freddata.head(5))
 print(freddata.describe())
 
-#importing csv using date column as index and sort 
+# importing csv using date column as index and sort
 nasdaq = pd.read_csv('Nasdaq.csv', parse_dates=['Date'], index_col='Date')
 sp500 = pd.read_csv('SP500.csv', parse_dates=['Date'], index_col='Date')
 dowjones = pd.read_csv('DowJones.csv', parse_dates=['Date'], index_col='Date')
@@ -23,5 +23,38 @@ print(dowjones.head(5))
 nasdaq.info()
 sp500.info()
 dowjones.info()
+
+#checking for empty info
+nasdaq_empty = nasdaq.isnull().sum()
+sp500_empty = sp500.isnull().sum()
+dowjones_empty = dowjones.isnull().sum()
+
+print([nasdaq_empty, sp500_empty, dowjones_empty])
+
+nas_sp = nasdaq.merge(sp500,
+                          on='Date',
+                          how='inner',
+                          suffixes=('_nasdaq', '_sp500')).dropna()
+nas_sp_dow = nas_sp.merge(dowjones, on='Date',
+                          how='inner').dropna()
+
+
+nas_sp_dow = nas_sp_dow.rename(columns={'Value_nasdaq': 'Nasdaq Val',
+                                        'Value_sp500': 'SP500 Val',
+                                        'Value': 'DowJones Val'})
+
+# checking merge outcome
+print([nas_sp, nas_sp_dow])
+
+# checking graph of all 3
+nas_sp_dow.plot(kind = 'line')
+plt.show()
+
+# normalising chart
+norm_nas_sp_dow = nas_sp_dow.div(nas_sp_dow.iloc[0]).mul(100)
+
+norm_nas_sp_dow.plot(kind= 'line',
+                     title='Normalized Stock Value')
+plt.show()
 
 

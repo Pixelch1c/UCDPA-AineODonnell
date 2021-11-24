a25e7be2d50eb893f329457d592fcfbcc27181d6
Pixelch1c <seisiunist@hotmail.com>
2021-11-24 13:52:29 +0000

    importing apis and from online.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,9 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas_datareader.data
-import DateTime
 from fredapi import Fred
 
 fred = Fred(api_key='52c96fa9db703fe0764c2589b15239d7')
-fred.get_series('BOGZ1FL073164003Q')
+freddata = fred.get_series('BOGZ1FL073164003Q')
+
+print(freddata.head(5))
+print(freddata.describe())
+
+nasdaq = pd.read_csv('Nasdaq.csv', parse_dates=['Date'], index_col='Date')
+sp500 = pd.read_csv('SP500.csv', parse_dates=['Date'], index_col='Date')
+Dowjones = pd.read_csv('DowJones.csv', parse_dates=['Date'], index_col='Date')
